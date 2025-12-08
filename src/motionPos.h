8d7c6614de4b7b9e78ba556688b1c8a8d34017b4
walte239 <walte239@purdue.edu>
2025-12-08 15:48:04 -0500

    Added oershoot

--- src/motionPos.h
+++ src/motionPos.h
@@ -11,7 +11,7 @@ const float STEPS_PER_REV   = 360.0f / STEP_ANGLE_DEG;
 const float DIST_PER_REV_MM = PULLEY_TEETH * BELT_PITCH_MM;
 const float STEPS_PER_MM    = (STEPS_PER_REV * MICROSTEP) / DIST_PER_REV_MM;
 
-const float OVERSHOOT_DIST = 2.0f;
+const float OVERSHOOT_DIST = 0.0f;
 
 
 //--------------------------------------------
